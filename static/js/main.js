2e5899c42fdeb640c01a9099881f86cc7cc1d142
Excellent58 <wycliffemusau99@gmail.com>
2024-06-21 17:24:10 +0300

    created navbar and other ui components

--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,10 @@
-const errorModal = document.querySelector('.err-modal');
+const dropDownSection = document.querySelector(".dropdown-sec")
+const dropDown = document.querySelector(".dropdown")
 
+dropDownSection.addEventListener("click", function () {
+    if(dropDown.classList.contains('hidden')) {
+        dropDown.classList.remove('hidden');
+    } else {
+        dropDown.classList.add('hidden');
+    }
+})
